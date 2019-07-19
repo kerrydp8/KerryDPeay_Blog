0dd4a97551c6c0db73d0c50d3b2b9d1256707470
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-07-19 16:58:21 -0400

    Index page is set up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KerryDPeay_Blog.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace KerryDPeay_Blog.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var publishedPosts = View(db.BlogPosts.Where(b => b.Published).ToList());
+            return View(publishedPosts);
         }
 
         public ActionResult About()
