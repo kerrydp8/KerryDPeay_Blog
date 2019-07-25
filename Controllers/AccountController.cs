fedc87ecdfc130ccc350c4071d904d8987df0269
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-07-25 12:36:00 -0400

    Logging out redirects to homepage

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -409,7 +409,7 @@ namespace KerryDPeay_Blog.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "BlogPosts");
         }
 
         //
