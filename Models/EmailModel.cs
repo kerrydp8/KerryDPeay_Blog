f12c4a977e6ce30a2a4122e619beeff93cdb734c
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-08-08 14:23:27 -0400

    Changed character requirement statement

--- Models/EmailModel.cs
+++ Models/EmailModel.cs
@@ -10,13 +10,13 @@ namespace KerryDPeay_Blog.Models
     public class EmailModel
     {
         [Required, Display(Name = "Name")]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} character long.", MinimumLength = 1)]
         public string FromName { get; set; }
         [Required, Display(Name = "Email"), EmailAddress]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} character long.", MinimumLength = 1)]
         public string FromEmail { get; set; }
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} character long.", MinimumLength = 1)]
         public string Subject { get; set; }
         [Required]
         [AllowHtml] //THIS IS NEEDED. Html must be allowed to use a RTE.
