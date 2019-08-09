e1012caeecf796669da43f2d934c7a55162c7faf
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-08-08 23:40:39 -0400

    New fields added to Register model

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -64,10 +64,24 @@ namespace KerryDPeay_Blog.Models
 
     public class RegisterViewModel
     {
+
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [EmailAddress]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [EmailAddress]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -12,7 +12,6 @@ namespace KerryDPeay_Blog.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string DisplayName { get; set; }
 
         //Virtual nav section
