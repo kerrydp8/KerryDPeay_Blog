438f2c8fda6f61fa2c7bdcf3d0a1c2b780a730f1
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-08-13 15:42:15 -0400

    Email requirement bug fixed

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -73,13 +73,11 @@ namespace KerryDPeay_Blog.Models
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
-        [EmailAddress]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
-        [EmailAddress]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
